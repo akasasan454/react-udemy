a7af6b50361da81399691edf26307a4da6ae1894
akasasan454 <go_aquarion.0454@icloud.com>
2019-11-26 17:50:41 +0900

    save

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 
 function App() {
   return (
-    <div>
-      <p>hoge</p>
-    </div>
+    <React.Fragment>
+      <div onClick={() => console.log("clicked")}>hogehoge</div>
+      <div onClick={() => console.log("clicked")}>hogehogehoge</div>
+    </React.Fragment>
   );
 }
 
