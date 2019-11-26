7ba1655305cd01983c5604bcf992813222804a3c
akasasan454 <go_aquarion.0454@icloud.com>
2019-11-26 22:54:53 +0900

    15.props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from "react";
 
 function App() {
+  const profiles = [
+    { name: "Taro", age: 20 },
+    { name: "Hanako", age: 12 },
+    { name: "NoName" }
+  ];
   return (
     <React.Fragment>
-      <div onClick={() => console.log("clicked")}>hogehoge</div>
-      <div onClick={() => console.log("clicked")}>hogehogehoge</div>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
     </React.Fragment>
   );
 }
+
+const User = props => {
+  return (
+    <div>
+      Hi, I am {props.name}, and {props.age} year old!
+    </div>
+  );
+};
+
+User.defaultProps = {
+  age: 100
+};
 
 export default App;
