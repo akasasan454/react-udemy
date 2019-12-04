7d91ddda1b5b8dc4db27653e7dad150c390b1bba
akasasan454 <go_aquarion.0454@icloud.com>
2019-12-04 21:15:33 +0900

    implement event new component

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -3,9 +3,13 @@ import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
 
-// import { postEvents } from "../actions";
+import { postEvent } from "../actions";
 
 class EventsNew extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
   renderField(field) {
     const {
       input,
@@ -14,15 +18,27 @@ class EventsNew extends Component {
       meta: { touched, error }
     } = field;
 
-    return <div></div>;
+    return (
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    );
   }
+
+  async onSubmit(values) {
+    await this.props.postEvent(values);
+    this.props.history.push("/");
+  }
+
   render() {
+    const { handleSubmit } = this.props;
     return (
       <React.Fragment>
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmit)}>
           <div>
             <Field
-              lebal="Title"
+              label="Title"
               name="title"
               type="text"
               component={this.renderField}
@@ -30,7 +46,7 @@ class EventsNew extends Component {
           </div>
           <div>
             <Field
-              lebal="Body"
+              label="Body"
               name="body"
               type="text"
               component={this.renderField}
@@ -48,12 +64,15 @@ class EventsNew extends Component {
 
 const validate = values => {
   const errors = {};
+
+  if (!values.title) errors.title = "Enter a title, please";
+  if (!values.body) errors.body = "Enter a body, please";
   return errors;
 };
 
-// const mapDispatchToProps = { postEvent };
+const mapDispatchToProps = { postEvent };
 
 export default connect(
   null,
-  null
+  mapDispatchToProps
 )(reduxForm({ validate, form: "eventNewForm" })(EventsNew));
